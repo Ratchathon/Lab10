e405ccdde5b2cf99d8e6b88b42f13bf97b1f9fa5
Ratchathon <Ratchathon_S@cmu.ac.th>
2024-12-20 14:44:07 +0700

    lab 10_2,3 finish

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,33 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<fstream>
+#include<cstdlib>
+#include<iomanip>
+using namespace std;
+
+
+
+int main()
+{
+    ifstream source;
+    source.open("score.txt");
+    string score;
+    ofstream dest;
+    dest.open("scorecopy.txt");
+    int i = 0;
+    float sum = 0,sum_of_square = 0;
+    while(getline(source,score))
+    {
+        dest<<i<<". "<<score<<endl;
+        i++;
+        sum +=stod(score.c_str());
+        sum_of_square +=pow(stod(score.c_str()),2);
+
+    }
+    cout<<"Number of data = "<<i<<endl;
+    cout<<setprecision(3);
+    cout<<"Mean = "<<sum/i<<"\n";
+    cout<<"Standard deviation = "<<sqrt(sum_of_square/i-pow(sum/i,2));
+}
+
